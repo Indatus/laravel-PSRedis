9776ec73c88aadc18706e21ce3ec2ed2c4dac095
Damien Russell <drussell@indatus.com>
2015-02-17 11:45:52 -0500

    updated phpunit xml config and brought test coverage up on driver to 100%

--- tests/DriverTest.php
+++ tests/DriverTest.php
@@ -13,69 +13,110 @@ use Config;
  */
 class DriverTest extends BaseTest
 {
+    /** @var \Indatus\LaravelPSRedis\Driver */
     protected $driverUnderTest;
+
+    /** @var array */
 
     protected $masters;
 
+    /** @var \Mockery\MockInterface */
     protected $masterDiscovery;
 
+    /** @var \Mockery\MockInterface */
     protected $HAClient;
 
+    /** @var \Mockery\MockInterface */
     protected $PSRedisClient;
 
     public function setUp()
     {
         parent::setUp();
 
+        // some of the classes from the PSRedis package implement the
+        // `__call()`  methods and define a default value for the method arguments
+        // php doesn't like while using the `E_STRICT` error level
+        if(defined('E_STRICT')) {
+            error_reporting('E_ALL ^ E_STRICT');
+        }
+
         $this->masters = [
             [
                 'host' => 'host1.domain.com',
-                'port' => '222222',
+                'port' => '222222'
             ],
             [
                 'host' => 'host2.domain.com',
-                'port' => '222222',
+                'port' => '222222'
             ]
         ];
 
         $this->masterDiscovery = m::mock('PSRedis\MasterDiscovery');
         $this->app->instance('PSRedis\MasterDiscovery', $this->masterDiscovery);
 
-        $this->HAClient = m::mock('PSRedis\HAClient');
+        $this->HAClient = m::mock('PSRedis\HAClient')->makePartial();;
         $this->app->instance('PSRedis\HAClient', $this->HAClient);
 
         $this->PSRedisClient = m::mock('PSRedis\Client');
         $this->app->instance('PSRedis\Client', $this->PSRedisClient);
-    }
 
-    /**
-     * @test
-     */
-    public function it_instantiates()
-    {
         Config::shouldReceive('get')->once()->with('Indatus/LaravelPSRedis::nodeSetName')->andReturn('node-set');
         Config::shouldReceive('get')->once()->with('Indatus/LaravelPSRedis::masters')->andReturn($this->masters);
 
+        $this->masterDiscovery->shouldReceive('addSentinel')->twice()->with($this->PSRedisClient);
+
         App::shouldReceive('make')
             ->once()
-            ->with('PSRedis\MasterDiscover', ['node-set'])
+            ->with('PSRedis\MasterDiscovery', ['node-set'])
             ->andReturn($this->masterDiscovery);
 
         App::shouldReceive('make')
             ->once()
-            ->with('PSRedis\Client', ['host1.domain.com', '222222'])
+            ->with('PSRedis\Client', [$this->masters[0]['host'], $this->masters[0]['port']])
             ->andReturn($this->PSRedisClient);
 
         App::shouldReceive('make')
             ->once()
-            ->with('PSRedis\Client', ['host2.domain.com', '222222'])
+            ->with('PSRedis\Client', [$this->masters[1]['host'], $this->masters[1]['port']])
             ->andReturn($this->PSRedisClient);
 
         App::shouldReceive('make')
             ->once()
             ->with('PSRedis\HAClient', [$this->masterDiscovery])
             ->andReturn($this->HAClient);
+    }
+
+    /**
+     * The instantiates method will run before each class to ensure
+     *
+     * @test
+     */
+    public function it_instantiates()
+    {
+        $this->driverUnderTest = new Driver();
+        $this->assertInstanceOf('Indatus\LaravelPSRedis\Driver', $this->driverUnderTest);
+    }
+
+    /**
+     * @test
+     */
+    public function it_gets_config_values()
+    {
+        $expectedConfig = [
+            'cluster' => false,
+            'default' => [
+                'host' => $this->masters[0]['host'],
+                'port' => $this->masters[0]['port']
+            ]
+        ];
+
+        Config::shouldReceive('get')->with('Indatus/LaravelPSRedis::cluster')->andReturn(false);
+        $this->HAClient->shouldReceive('getIpAddress')->once()->andReturn($this->masters[0]['host']);
+        $this->HAClient->shouldReceive('getPort')->once()->andReturn($this->masters[0]['port']);
 
         $this->driverUnderTest = new Driver();
+        $configUnderTest = $this->driverUnderTest->getConfig();
+
+        $this->assertEquals($expectedConfig, $configUnderTest);
     }
 }
